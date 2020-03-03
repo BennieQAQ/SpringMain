c0c213e9aa5555c86474b247ceaa778144f8ac0f
BennieQAQ <1282811622@qq.com>
2020-03-03 19:14:45 +0800

    111

---
+++ src/testmain/HelloWord.java
@@ -0,0 +1,36 @@
+package testmain;
+
+public class HelloWord {
+
+	private String name;
+	private int age;
+	private boolean gender;
+	private String Study_number;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public boolean isGender() {
+		return gender;
+	}
+	public void setGender(boolean gender) {
+		this.gender = gender;
+	}
+	public String getStudy_number() {
+		return Study_number;
+	}
+	public void setStudy_number(String study_number) {
+		Study_number = study_number;
+	}
+	
+	
+
+}
